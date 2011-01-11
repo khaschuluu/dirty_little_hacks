2907ec6c92f3dd722d1130c13705bb170a74fe25
khasaa@t60 <khaschuluu.m@gmail.com>
2011-01-11 22:25:16 +0800

    Fixed l^Cle hack for get valuite from GolomtBank

--- golomtbank.sh
+++ golomtbank.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-curl "http://www.golomtbank.com/whansh.aspx?lang=1"	|\
-	grep "<table[^>]*>.*</table>" 					|\
-	sed -re "s|<tr[^>]*>|\n|g"						\
-	-e "s|<td[^>]*><img[^>]*></td><td[^>]*>||g"		\
-	-e "s|</td><td[^>]*>| |g"						\ 
-	-e "s|</td></tr>||g" 							\
-	-e "s|<td.*||g"									\
+curl "http://www.golomtbank.com/whansh.aspx?lang=1" |\
+	grep "<table[^>]*>.*</table>" |\
+	sed -re "s|<tr[^>]*>|\n|g" \
+	-e "s|<td[^>]*><img[^>]*></td><td[^>]*>||g" \
+	-e "s|</td><td[^>]*>| |g" \
+	-e "s|</td></tr>||g" \
+	-e "s|<td.*||g" \
+	-e "s| <a.*||g"
